b1b686103c03944c978ac56d217d2ddda90bf052
chuongCode <chuongrichard@gmail.com>
2025-04-28 05:51:15 -0400

    Prototype of the login page

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,4 @@
-// Main JavaScript file
-
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Flask application loaded successfully');
     
-    // Add any JavaScript functionality here
 }); 
