cdc985ee913a049acb13ae0dab8f711b033515d9
chuongCode <chuongrichard@gmail.com>
2025-04-28 15:53:15 -0400

    Renamed buncha stuff, proper Log In MVP is done, compartmentalized some components that are reused, former log in page is now called home page

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        # Handle login logic here
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # For now, just redirect to home after login attempt
+        return redirect(url_for('home'))
     return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    # Placeholder signup page
+    return '<h1>Signup Page Coming Soon!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True) 
