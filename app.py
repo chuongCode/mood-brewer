8675679fa66ca82dead6066bdc348b41727a1b50
Emkyrie <144286497+Emkyrie@users.noreply.github.com>
2025-04-29 00:44:18 -0400

    made home page and animated the landing page

--- app.py
+++ app.py
@@ -3,45 +3,32 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def landing():
     return render_template('landing.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # Handle login logic here
         username = request.form.get('username')
         password = request.form.get('password')
-        # For now, just redirect to home after login attempt
-        return redirect(url_for('home'))
     return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        # Get form data
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
-        
-        # TODO: Add validation and error handling
-        # TODO: Add user to database
-        # TODO: Handle password hashing
-        
-        # For now, just redirect to login page after signup
         return redirect(url_for('login'))
-    
     return render_template('signup.html')
 
 @app.route('/profile')
 def profile():
-    # TODO: Add authentication check
-    # TODO: Fetch user data from database
     return render_template('profile.html')
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
